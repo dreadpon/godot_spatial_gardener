dreadpon/godot_spatial_gardener

commit 902fe9c1e4de209b2d9d867e297335c14cc40e91

Fix Issue #48: Arborist loses all data upon saving/changing scenes

addons/dreadpon.spatial_gardener/arborist/mmi_octree/mmi_octree_node.gd
@@ -130,20 +130,6 @@ func safe_init_root():
 	octant = -1
 
 
-# Cleanup this this node before deletion
-# TODO: find out if I can clear the member array here as well
-func prepare_for_removal():
-	print_address("", "prepare for removal")
-	
-	# Avoid circular reference so that RefCount can properly free objects
-	parent = null
-	for child in child_nodes:
-		child.prepare_for_removal()
-	child_nodes = []
-	
-	set_is_leaf(false)
-
-
 # Restore any states that might be broken after loading this node
 func restore_after_load(__MMI_container:Node3D, LOD_variants:Array):
 	MMI_container = __MMI_container
@@ -192,7 +178,29 @@ func set_is_leaf(val):
 	active_LOD_index = -1
 
 
-func destroy():
+# Cleanup this this node before deletion
+# TODO: find out if I can clear the member array here as well
+func prepare_for_removal():
+	print_address("", "prepare for removal")
+	
+	# Avoid circular reference so that RefCount can properly free objects
+	parent = null
+	MMI_container = null
+	MMI = null
+	MMI_multimesh = null
+	for child in child_nodes:
+		child.prepare_for_removal()
+	child_nodes = []
+	
+	set_is_leaf(false)
+
+
+# Free anything that might incur a circular reference or a memory leak
+# Anything that is @export'ed is NOT touched here
+# We count on Godot's own systems to handle that in whatever way works best
+# TODO: this is very similar to prepare_for_removal(), need to determine how best to combine the two
+#		will need to happen around v2.0.0, since it's a very risky change
+func free_refs():
 	member_placeforms = []
 	parent = null
 	MMI_container = null
@@ -201,11 +209,7 @@ func destroy():
 	shared_LOD_variants = []
 	
 	for child in child_nodes:
-		child.destroy()
-	child_nodes = []
-	reset_member_arrays()
-	# if is_instance_valid(MMI) && MMI.is_inside_tree():
-	# 	MMI.queue_free()
+		child.free_refs()
 
 
 

addons/dreadpon.spatial_gardener/arborist/arborist.gd
@@ -125,7 +125,7 @@ func _notification(what):
 	match what:
 		NOTIFICATION_PREDELETE:
 			for octree_manager in octree_managers:
-				octree_manager.destroy()
+				octree_manager.free_refs()
 
 
 func _exit_tree():
@@ -137,7 +137,6 @@ func _exit_tree():
 	#if Engine.is_editor_hint(): return
 	#for octree_manager in octree_managers:
 		#octree_manager.destroy()
-#	octree_managers = []
 #	mutex_placement.lock()
 #	exit_instance_placement = true
 #	done_instance_placement = false

addons/dreadpon.spatial_gardener/arborist/mmi_octree/mmi_octree_manager.gd
@@ -86,11 +86,6 @@ func init_octree(members_per_node:int, root_extent:float, center:Vector3 = Vecto
 	request_debug_redraw()
 
 
-func prepare_for_removal():
-	if root_octree_node:
-		root_octree_node.prepare_for_removal()
-
-
 func connect_node(octree_node:MMIOctreeNode):
 	assert(octree_node)
 	FunLib.ensure_signal(octree_node.placeforms_rejected, grow_to_members)
@@ -105,10 +100,21 @@ func disconnect_node(octree_node:MMIOctreeNode):
 	octree_node.req_debug_redraw.disconnect(request_debug_redraw)
 
 
-func destroy():
+func prepare_for_removal():
+	if root_octree_node:
+		root_octree_node.prepare_for_removal()
+
+
+# Free anything that might incur a circular reference or a memory leak
+# Anything that is @export'ed is NOT touched here
+# We count on Godot's own systems to handle that in whatever way works best
+# TODO: this is very similar to prepare_for_removal(), need to determine how best to combine the two
+#		will need to happen around v2.0.0, since it's a very risky change
+func free_refs():
+	print(self, " ", resource_local_to_scene
+, " ", get_local_scene())
 	if !root_octree_node: return
-	root_octree_node.destroy()
-	root_octree_node = null
+	root_octree_node.free_refs()
 
 
 

commit 729b4131b54053417e8d69a136d02dd7625bdf7f

Fix regression in Multimesh management caused by 902fe9c

addons/dreadpon.spatial_gardener/arborist/mmi_octree/mmi_octree_node.gd
@@ -185,9 +185,6 @@ func prepare_for_removal():
 	
 	# Avoid circular reference so that RefCount can properly free objects
 	parent = null
-	MMI_container = null
-	MMI = null
-	MMI_multimesh = null
 	for child in child_nodes:
 		child.prepare_for_removal()
 	child_nodes = []