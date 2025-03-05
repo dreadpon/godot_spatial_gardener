40da85454fc9572492e0a1b90652be23fe50681c
Greg Dreadpon <dreadpon@gmail.com>
2025-03-05 10:50:44 +0300

    Manage EditorInterface through FeatureManager

--- addons/dreadpon.spatial_gardener/utility/editor_interface_interface.gd
+++ addons/dreadpon.spatial_gardener/utility/editor_interface_interface.gd
@@ -14,22 +14,22 @@ static var _selected_nodes: Array[Node3D]
 
 static func get_current_viewport():
 	if Engine.is_editor_hint():
-		return EditorInterface.get_editor_viewport_3d(0)
+		return DPON_FM.ED_EditorInterface.get_editor_viewport_3d(0)
 	
 	return SceneTree.root.get_viewport()
 
 
 static func get_selected_nodes():
 	if Engine.is_editor_hint():
-		return EditorInterface.get_selection().get_selected_nodes()
+		return DPON_FM.ED_EditorInterface.get_selection().get_selected_nodes()
 	
 	return _selected_nodes
 
 
 static func select_single_node(p_node: Node3D):
 	if Engine.is_editor_hint():
-		EditorInterface.get_selection().clear()
-		EditorInterface.get_selection().add_node(p_node)
+		DPON_FM.ED_EditorInterface.get_selection().clear()
+		DPON_FM.ED_EditorInterface.get_selection().add_node(p_node)
 		return
 	
 	_selected_nodes.clear()
@@ -38,5 +38,5 @@ static func select_single_node(p_node: Node3D):
 
 static func get_ui_scale() -> float:
 	if Engine.is_editor_hint():
-		return EditorInterface.get_editor_scale()
+		return DPON_FM.ED_EditorInterface.get_editor_scale()
 	return 1.0
--- addons/dreadpon.spatial_gardener/utility/feature_manager.gd
+++ addons/dreadpon.spatial_gardener/utility/feature_manager.gd
@@ -3,6 +3,7 @@ class_name DPON_FM
 
 static var ED_EditorUndoRedoManager = null
 static var ED_EditorFileDialog = null
+static var ED_EditorInterface = null
 
 static var _class_map: Dictionary = {}
 
@@ -12,12 +13,15 @@ static var _class_map: Dictionary = {}
 static func setup():
 	ED_EditorUndoRedoManager = get_native_class("EditorUndoRedoManager")
 	ED_EditorFileDialog = get_native_class("EditorFileDialog")
+	ED_EditorInterface = get_native_class("EditorInterface")
 	
 	_class_map = {}
 	if ED_EditorUndoRedoManager:
 		_class_map["EditorUndoRedoManager"] = ED_EditorUndoRedoManager
 	if ED_EditorFileDialog:
 		_class_map["EditorFileDialog"] = ED_EditorFileDialog
+	if ED_EditorInterface:
+		_class_map["EditorInterface"] = ED_EditorInterface
 
 
 static func is_instance_of_ed(instance: Variant, str_lass_name: String) -> bool:
